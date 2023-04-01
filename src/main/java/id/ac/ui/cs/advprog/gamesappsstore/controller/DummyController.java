9c508d77154bbda7f3a38f7baaf0efbf2f36bc47
emir.shamsuddin <emir.shamsuddin@ui.ac.id>
2023-04-01 22:48:08 +0700

    feat: add storage

--- src/main/java/id/ac/ui/cs/advprog/gamesappsstore/controller/DummyController.java
+++ src/main/java/id/ac/ui/cs/advprog/gamesappsstore/controller/DummyController.java
@@ -1,5 +1,8 @@
 package id.ac.ui.cs.advprog.gamesappsstore.controller;
 
+import id.ac.ui.cs.advprog.gamesappsstore.storage.Storage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/dummy")
 public class DummyController {
-
     @GetMapping("/test1")
     public ResponseEntity<String> test1() {
         return ResponseEntity.ok("Test1");
---
+++ src/main/java/id/ac/ui/cs/advprog/gamesappsstore/exceptions/ServerException.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.gamesappsstore.exceptions;
+
+public class ServerException extends RuntimeException {
+    public ServerException(String message) {
+        super(message);
+    }
+}
