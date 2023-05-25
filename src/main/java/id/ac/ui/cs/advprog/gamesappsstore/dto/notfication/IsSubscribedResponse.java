f003130ce707415acd3102b9de3bf300321fa272
emir.shamsuddin <emir.shamsuddin@ui.ac.id>
2023-05-25 22:08:31 +0700

    fix: consistent naming

--- src/main/java/id/ac/ui/cs/advprog/gamesappsstore/dto/notfication/IsSubscribedResponse.java
+++ src/main/java/id/ac/ui/cs/advprog/gamesappsstore/dto/notfication/IsSubscribedResponse.java
@@ -8,5 +8,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class IsSubscribedResponse {
-    private boolean isSubscribed;
+    private boolean subscribedStatus;
 }
