bf0600d8043c323eeeb6bb0d6a01183d492b1c53
emir.shamsuddin <emir.shamsuddin@ui.ac.id>
2023-05-25 21:18:26 +0700

    refactor: observer

--- src/main/java/id/ac/ui/cs/advprog/gamesappsstore/core/notification/Observer.java
+++ src/main/java/id/ac/ui/cs/advprog/gamesappsstore/core/notification/Observer.java
@@ -2,6 +2,4 @@ package id.ac.ui.cs.advprog.gamesappsstore.core.notification;
 
 import id.ac.ui.cs.advprog.gamesappsstore.models.notification.NotificationData;
 
-public interface Observer {
-    void handleNotification(NotificationData notificationData);
-}
+public interface Observer {}
